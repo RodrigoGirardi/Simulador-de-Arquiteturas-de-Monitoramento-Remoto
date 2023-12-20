5fba4c590c70c773cbc1d50fcc3e8e18aca877b1
RodrigoGirardi <102379861+RodrigoGirardi@users.noreply.github.com>
2023-12-20 17:44:48 -0300

    Delete Instituto Butantan directory

--- Instituto Butantan/Socket/Transf_de_Arq/TESTES/leitura.py
+++
@@ -1,9 +0,0 @@
-with open('recebido.txt', 'r') as arquivo:
-    # Use a função next() para ler a primeira linha
-    primeira_linha = next(arquivo)
-    
-    # Use a função next() novamente para ler a segunda linha
-    segunda_linha = next(arquivo)
-x = 'ÝÝ»»ÌÌ'
-bytes_hex = x.encode('utf-8').hex()
-print (bytes_hex)
